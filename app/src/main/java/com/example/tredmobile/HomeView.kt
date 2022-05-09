c4cfce778505831ac4ca684ab54b40f7b4de4376
Matthew Sullivan <matt54633@gmail.com>
2022-05-09 13:03:13 +0100

    Basic View Started, ready for development now

--- app/src/main/java/com/example/tredmobile/HomeView.kt
+++ app/src/main/java/com/example/tredmobile/HomeView.kt
@@ -51,8 +51,6 @@ class HomeView: View {
         color = Color.rgb(248, 243, 243)
     }
 
-     val paint: Paint = Paint()
-
     //canvas size
     private val canvasHeight: Float
         get() = height.toFloat()
@@ -66,16 +64,21 @@ class HomeView: View {
 
     //onDraw function
     override fun onDraw(canvas: Canvas) {
-        val heightDivider: Float = canvasHeight / 30f
-
         canvas.drawRect(0f, 0f, canvasWidth, canvasHeight, backgroundPaint)
+        drawTopBar(canvas)
+        drawCards(canvas)
+    }
+
+    private fun drawTopBar(canvas: Canvas) {
         canvas.drawRect(0f, 0f, canvasWidth, canvasHeight / 12f, snowPaint)
         canvas.drawText("- Today -", canvasWidth / 2f, canvasHeight / 17f, orangeTextPaint)
+    }
+
+    private fun drawCards(canvas: Canvas) {
+        val heightDivider: Float = canvasHeight / 30f
         canvas.drawRoundRect(canvasWidth / 30f, heightDivider * 4, canvasWidth - (canvasWidth / 30f), heightDivider * 9, 30f, 30f, cardPaint)
         canvas.drawRoundRect(canvasWidth / 30f, heightDivider * 10, canvasWidth - (canvasWidth / 30f), heightDivider * 15, 30f, 30f, cardPaint)
         canvas.drawRoundRect(canvasWidth / 30f, heightDivider * 16, canvasWidth - (canvasWidth / 30f), heightDivider * 22, 30f, 30f, cardPaint)
         canvas.drawRoundRect(canvasWidth / 30f, heightDivider * 23, canvasWidth - (canvasWidth / 30f), heightDivider * 29, 30f, 30f, cardPaint)
-
     }
-
-    }
+}
