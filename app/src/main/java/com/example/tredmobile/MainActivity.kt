ae2991e42ed99b4e18ff04b356cf177a3bce1034
Jacob Stone <jacob.rhys.stone@gmail.com>
2022-05-11 14:09:36 +0100

    added transition functionality from main view to settings view

--- app/src/main/java/com/example/tredmobile/MainActivity.kt
+++ app/src/main/java/com/example/tredmobile/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tredmobile
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsButton = findViewById<ImageView>(R.id.settingsLogo)
+        settingsButton.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
+    
 }
