7e21c44d7a66fa8f2d95d2be39f7638ccd5e9de8
Jacob Stone <jacob.rhys.stone@gmail.com>
2022-05-11 15:23:11 +0100

    Home button from settings to home now works

--- app/src/main/java/com/example/tredmobile/SettingsActivity.kt
+++ app/src/main/java/com/example/tredmobile/SettingsActivity.kt
@@ -1,11 +1,19 @@
 package com.example.tredmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val homeButton = findViewById<ImageView>(R.id.homeLogo)
+        homeButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
