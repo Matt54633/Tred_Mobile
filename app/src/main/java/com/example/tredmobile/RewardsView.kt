4001588f8d292ecd2535ed7649cf59582a5b1c54
Matthew Sullivan <matt54633@gmail.com>
2022-05-12 14:09:37 +0100

    Completed RewardsView.kt and activity_rewards.xml

---
+++ app/src/main/java/com/example/tredmobile/RewardsView.kt
@@ -0,0 +1,117 @@
+package com.example.tredmobile
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.View
+
+class RewardsView: View {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    //scale value
+    private val scaleValue = context.resources.displayMetrics.density
+
+    //paints
+    //background paint
+    private val backgroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(41, 41, 41)
+        textSize = 20f * scaleValue
+        typeface = Typeface.SANS_SERIF
+        isFakeBoldText = true
+    }
+
+    private val cardPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(59, 59, 59)
+    }
+
+    private val orangePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(255, 107, 0)
+    }
+
+    private val orangeTextPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.rgb(255, 107, 0)
+        textAlign = Paint.Align.CENTER
+        textSize = 30f * scaleValue
+        typeface = Typeface.SANS_SERIF
+        isFakeBoldText = true
+    }
+
+    private val bluePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(25, 123, 189)
+    }
+
+    private val snowPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(248, 243, 243)
+        textSize = 20f * scaleValue
+        typeface = Typeface.SANS_SERIF
+        isFakeBoldText = true
+    }
+
+    private val snowPaintSmall: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.rgb(248, 243, 243)
+        textSize = 15f * scaleValue
+        typeface = Typeface.SANS_SERIF
+    }
+
+    private val boldNavPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 28f
+        color = Color.rgb(248, 243, 243)
+    }
+
+    private val navPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 28f
+        color = Color.rgb(117, 117, 117)
+    }
+
+    //canvas size
+    private val canvasHeight: Float
+        get() = height.toFloat()
+    private val canvasWidth: Float
+        get() = width.toFloat()
+
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawRect(0f, 0f, canvasWidth, canvasHeight, backgroundPaint)
+        drawTopBar(canvas)
+        drawCards(canvas)
+        drawBottomNav(canvas)
+    }
+
+    private fun drawTopBar(canvas: Canvas) {
+        canvas.drawRect(0f, 0f, canvasWidth, canvasHeight / 12f, snowPaint)
+        canvas.drawText("- Rewards -", canvasWidth / 2f, canvasHeight / 17f, orangeTextPaint)
+    }
+
+    private fun drawCards(canvas: Canvas) {
+        val heightDivider: Float = canvasHeight / 30f
+        //theme card
+        canvas.drawRoundRect(canvasWidth / 30f, heightDivider * 3, canvasWidth - (canvasWidth / 30f), heightDivider * 28, 30f, 30f, cardPaint)
+    }
+    private fun drawBottomNav(canvas: Canvas) {
+        val xSep: Float  = canvasWidth / 20f
+        val offset: Float = canvasWidth / 2.22f
+        var dotPaint: Paint
+        for (i in 0 until 3) {
+            dotPaint = if (i != 2) {
+                navPaint
+            } else {
+                boldNavPaint
+            }
+            canvas.drawPoint((i * xSep) + offset, canvasHeight - (canvasHeight / 35f), dotPaint)
+        }
+    }
+}
